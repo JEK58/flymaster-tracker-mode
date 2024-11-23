813329a49b23dc5c251812b1551c4558129e3e29
Stephan Schöpe <socialcrap+github@posteo.de>
2024-11-23 09:16:07 +0100

    password note added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import DeactivateForm from "@/components/RegistrationForm";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,6 +7,17 @@ export default function Home() {
       <h1 className="text-2xl font-bold text-center mb-6 mt-6">
         Deactivate comp mode
       </h1>
+      <p className="max-w-md mx-auto my-6">
+        You should never enter your flymaster password anywhere except on the
+        flymaster website. If you trust me, you can still to do it and use this
+        tool. The password is not saved. Convince youself:{" "}
+        <Link
+          href="https://github.com/JEK58/flymaster-tracker-mode"
+          className="underline"
+        >
+          Source Code
+        </Link>
+      </p>
       <DeactivateForm />
     </main>
   );
